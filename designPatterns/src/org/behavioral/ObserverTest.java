22e18f1c05a6f3d4dd0f751db1b283d74c25010c
dim912 <dim912@gmail.com>
2018-02-19 00:55:44 +0800

    Design Pattern : Observer

---
+++ designPatterns/src/org/behavioral/ObserverTest.java
@@ -0,0 +1,98 @@
+package org.behavioral;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implements the one to many way of message passing. Java has Observerbale class and Observer interface for this design pattern in mind
+ *
+ * Observable does not couple with the observers. But deal through an interface.
+ *
+ * can be used to implement => subscribe / publish model
+ *
+ * DownSide : May cause to memory leaks if forget to remove the observers from observerList.
+ *
+ *
+ * */
+
+public class ObserverTest {
+
+    public static void main(String[] agrs){
+
+        Item shirt = new Item(); shirt.setItemName("Shirt");
+        Customer cust1 = new Customer(); cust1.setName("Cust1");
+        Customer cust2 = new Customer(); cust2.setName("Cust2");
+
+        shirt.addObserver(cust1);
+        shirt.addObserver(cust2);
+
+        shirt.notifyObservers("Price reduced to 21");
+
+    }
+}
+
+interface  Observer{
+    void update(Observable observable, Object message);
+}
+
+interface  Observable{
+    void addObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers(Object message);
+}
+
+class Item implements  Observable{
+
+    private String itemName;
+
+    List<Observer> observerList = new ArrayList<>();
+
+    @Override
+    public void addObserver(Observer observer) {
+        if(!observerList.contains(observer)){
+            observerList.add(observer);
+        }
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observerList.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers(Object message) {
+        observerList.forEach(o-> o.update(this,message));
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+
+    @Override
+    public String toString() {
+        return itemName;
+    }
+}
+
+class Customer implements  Observer {
+
+    private String name;
+
+    @Override
+    public void update(Observable observable, Object message) {
+        System.out.println("Got notified from : " +  observable.toString() + " : "  + message.toString());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
