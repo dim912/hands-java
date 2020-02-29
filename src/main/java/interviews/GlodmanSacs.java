3c41d06b3fb3da3513a4ee3d422d3863442120d0
dim912 <dim912@gmail.com>
2020-02-29 15:08:43 +0800

    updated

---
+++ src/main/java/interviews/GlodmanSacs.java
@@ -0,0 +1,30 @@
+package interviews;
+
+public class GlodmanSacs {
+}
+
+
+/**
+ * coder pad 1
+ * -----------
+ *
+ * String  text = "asjhfaufewe"
+ *
+ * find the missing alphabetical letters of the text, respect to all 26 letters of english  alphabet
+ *
+ * => sol : push letters in the text to a HashSet. Search on the set for all 26 letters. If not present add into a StringBuilder
+ *
+ * coder pad 2
+ * -----------
+ *
+ *  marks[][] =  [ ["aladeen", 23], ["sebastinan", 34], ["anthoney", 23], ["aladeen" , 56], ...............]
+ *
+ * Find the student who has the maximum average
+ *
+ * => Sol : create a class Mark {  total; numberOfSubjects}
+ *
+ * => load all the students marks to a HashMap. Name as the key and Mark object as the value.
+ *
+ * Then iterate through all the entries of the map to find the maximum average
+ *
+ * */
